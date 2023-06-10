1dff4e53e18f5e2bdd91f585dad691990cd34432
Shubham Singh <shubham.itpiet@gmail.com>
2023-06-10 12:50:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { useEffect, useState } from 'react';
 function App() {
+  const[rating,setRating]=useState()
+  const[comment,setComment]=useState("")
+
+  const handleRatingChange=(star)=>{
+    setRating(star)
+  }
+  useEffect(()=>{
+      if(rating<3){setComment("poor")}
+      else if(rating===3){setComment("fair")}
+      else if(rating>3){setComment("Good")}
+      else{setComment("not given")}
+  },[rating])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h3>Your current rating score is: {rating}</h3>
+<h1>Rating:{comment}</h1>
+ <div>  {  [1,2,3,4,5].map((star)=>(
+<span className="stars"
+key={star}
+onClick={()=>{handleRatingChange(star)}}
+style={{color:star<=rating?'yellow':'gray',cursor:'pointer'}}
+>
+  <FontAwesomeIcon icon={faStar}/>
+</span>
+     ))}
+     </div>
     </div>
   );
 }
